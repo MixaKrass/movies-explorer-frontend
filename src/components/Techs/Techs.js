7779b6329ca6440ed8e58d9bc0b5b20368725650
Riddler <2584275@gmail.com>
2021-09-14 17:59:23 +0300

    AboutMe done

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -4,7 +4,18 @@ import './Techs.css'
 function Techs() {
   return (
     <section className="techs">
-      
+        <h2 className='techs-title'>Технологии</h2>
+        <h3 className='techs-subtitle'>7 технологий</h3>
+        <h2 className='techs__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</h2>
+          <ul className='techs__researched'>
+            <li className='techs__researched-item'>HTML</li>
+            <li className='techs__researched-item'>CSS</li>
+            <li className='techs__researched-item'>JS</li>
+            <li className='techs__researched-item'>React</li>
+            <li className='techs__researched-item'>Git</li>
+            <li className='techs__researched-item'>Express.js</li>
+            <li className='techs__researched-item'>mongoDB</li>
+          </ul>
     </section>
   )
 }
