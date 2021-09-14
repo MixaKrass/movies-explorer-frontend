d6d67ddaf48c75ff2bd7a9287559ec2e3b9ea7e1
Riddler <2584275@gmail.com>
2021-09-14 19:25:21 +0300

    landing done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Main />
+      <Footer />
     </div>
   )
 }
